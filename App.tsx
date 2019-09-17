238ba412a559b658b0a9d47da00f5abf7a5b864d
solgamd <solgamd@gmail.com>
2019-09-17 13:19:28 -0500

    Added ugly styling to experiment

--- App.tsx
+++ App.tsx
@@ -8,9 +8,14 @@ export default class App extends React.Component<AppProps, AppState> {
   render() {
     return (
       <>
-        <View style={styles.container}>
-          <Text style={styles.textStyleHeader}>This is a header</Text>
-          <Text style={styles.textStyleSub}>This is some smaller text</Text>
+        <View style={styles.container1}>
+          <View style={styles.container2}>
+            <View style={styles.container3}>
+              <Text style={styles.textStyleHeader}>#crushingit</Text>
+              <Text style={styles.textStyleSub}>An app to show how awesome</Text>
+              <Text style={styles.textStyleSub}>I am at React Native 😎</Text>
+            </View>
+          </View>
         </View>
       </>
     );
@@ -18,18 +23,31 @@ export default class App extends React.Component<AppProps, AppState> {
 };
 
 const styles = StyleSheet.create({
-  container: {
+  container1: {
     flex: 1,
-    backgroundColor: '#00a9e0',
+    backgroundColor: 'red',
+    padding: 20
+  },
+  container2: {
+    flex: 1,
+    backgroundColor: 'black',
+    padding: 20
+  },
+  container3: {
+    flex: 1,
+    backgroundColor: 'lightgreen',
     alignItems: 'center',
     justifyContent: 'center',
   },
   textStyleHeader: {
-    fontSize: 30,
-    margin: 2
+    fontSize: 40,
+    margin: 2,
+    color: 'white',
+    backgroundColor: 'red'
   },
   textStyleSub: {
     fontSize: 15,
-    margin: 2
+    margin: 3,
+    fontFamily: 'American Typewriter'
   }
 });
