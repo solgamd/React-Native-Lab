d50650976e726e12f2b03b0d2b3153afb630753d
solgamd <solgamd@gmail.com>
2019-09-17 15:56:10 -0500

    Added functional JSX Elements

--- App.tsx
+++ App.tsx
@@ -1,23 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
 interface AppProps { }
 interface AppState { }
 
 export default class App extends React.Component<AppProps, AppState> {
+
+  buttonOne = () => {
+    Alert.alert('Awesome! Keep on rockin!')
+  }
+
+  buttonTwo = () => {
+    Alert.alert('That sucks, bud. Do smething nice for yourself today!')
+  }
+
   render() {
     return (
-      <>
-        <View style={styles.container1}>
-          <View style={styles.container2}>
-            <View style={styles.container3}>
-              <Text style={styles.textStyleHeader}>#crushingit</Text>
-              <Text style={styles.textStyleSub}>An app to show how awesome</Text>
-              <Text style={styles.textStyleSub}>I am at React Native 😎</Text>
-            </View>
+
+      <View style={styles.container1}>
+        <View style={styles.container2}>
+          <View style={styles.container3}>
+            <Text style={styles.textStyleHeader}>How are you feeling?</Text>
+
+            <Button
+              color="#CE796B"
+              accessibilityLabel="Button: 'Pretty Good!'"
+              title="Pretty good!"
+              onPress={this.buttonOne} />
+
+            <Button
+              color="#495867"
+              accessibilityLabel="Button: 'Not so great."
+              title="Not so great."
+              onPress={this.buttonTwo} />
           </View>
         </View>
-      </>
+      </View>
     );
   }
 };
@@ -25,29 +43,27 @@ export default class App extends React.Component<AppProps, AppState> {
 const styles = StyleSheet.create({
   container1: {
     flex: 1,
-    backgroundColor: 'red',
-    padding: 20
+    backgroundColor: '#E7AD99',
+    padding: 5,
   },
   container2: {
     flex: 1,
-    backgroundColor: 'black',
-    padding: 20
+    backgroundColor: '#C18C5D',
+    padding: 5
   },
   container3: {
     flex: 1,
-    backgroundColor: 'lightgreen',
+    backgroundColor: '#ECC8AF',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
   textStyleHeader: {
     fontSize: 40,
-    margin: 2,
-    color: 'white',
-    backgroundColor: 'red'
-  },
-  textStyleSub: {
-    fontSize: 15,
-    margin: 3,
-    fontFamily: 'American Typewriter'
+    marginHorizontal: 5,
+    marginBottom: 50,
+    color: 'slategray',
+    fontFamily: 'American Typewriter',
+    fontWeight: 'bold'
   }
 });
